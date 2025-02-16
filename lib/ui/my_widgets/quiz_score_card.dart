d546d0cec1d5f35892e033dabd53c7b0b684f7ab
luai0yk <loaikalesi@gmail.com>
2025-02-16 13:15:51 +0300

    Added QuizScoreCard widget and implemented quiz scores tab

---
+++ lib/ui/my_widgets/quiz_score_card.dart
@@ -0,0 +1,121 @@
+import 'package:ewords/models/quiz_score_model.dart';
+import 'package:ewords/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../theme/my_colors.dart';
+
+class QuizScoreCard extends StatelessWidget {
+  final QuizScoreModel quizScore;
+  const QuizScoreCard({super.key, required this.quizScore});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.sp),
+      margin: EdgeInsets.only(bottom: 8.sp),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.onSurface,
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(width: 1, color: Colors.black12),
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: MyColors.themeColors[50],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  MyConstants.levels[quizScore.bookId],
+                  style: TextStyle(
+                    color: MyColors.themeColors[300],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 5),
+              Container(
+                decoration: BoxDecoration(
+                  color: MyColors.themeColors[50],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  'Unit: ${quizScore.unitId}',
+                  style: TextStyle(
+                    color: MyColors.themeColors[300],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Stack(
+            alignment: Alignment.centerLeft,
+            children: [
+              LinearProgressIndicator(
+                color: MyColors.themeColors[300],
+                backgroundColor: MyColors.themeColors[50],
+                value: quizScore.totalScore / 100,
+                minHeight: 15.h,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              Text(
+                '  Score ${quizScore.totalScore.toInt()}%',
+                style: const TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          Stack(
+            alignment: Alignment.centerLeft,
+            children: [
+              LinearProgressIndicator(
+                color: Colors.green[300],
+                backgroundColor: Colors.green[50],
+                value: quizScore.correctAnswers / 20,
+                minHeight: 15.h,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              Text(
+                '  Correct ${quizScore.correctAnswers}',
+                style: const TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          Stack(
+            alignment: Alignment.centerLeft,
+            children: [
+              LinearProgressIndicator(
+                color: Colors.red[300],
+                backgroundColor: Colors.red[50],
+                value: quizScore.wrongAnswers / 20,
+                minHeight: 15.h,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              Text(
+                '  Wrong ${quizScore.wrongAnswers}',
+                style: const TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/tabs/quiz_scores.dart
@@ -0,0 +1,83 @@
+import 'package:ewords/db/quiz_score_helper.dart';
+import 'package:ewords/models/quiz_score_model.dart';
+import 'package:ewords/ui/my_widgets/quiz_score_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hidable/hidable.dart';
+
+import '../../theme/my_colors.dart';
+
+class Quiz extends StatefulWidget {
+  final ScrollController scrollController;
+  const Quiz({super.key, required this.scrollController});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: Hidable(
+        deltaFactor: 0.06,
+        preferredWidgetSize: Size.fromHeight(75.sp),
+        controller: widget.scrollController,
+        child: AppBar(
+          title: Text('quiz scores'.toUpperCase()), // Title of the app bar
+          titleTextStyle: TextStyle(
+            color: MyColors.themeColors[300],
+            fontSize: 16.sp,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+          ),
+        ),
+      ),
+      body: FutureBuilder<List<QuizScoreModel>>(
+        future: QuizScoreHelper.instance.getQuizScores(),
+        builder: (context, snapshot) {
+          // Handle loading state
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+                child: CircularProgressIndicator(strokeWidth: 10));
+          }
+          // Handle error state
+          else if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Sorry, something went wrong!',
+                style: TextStyle(
+                  color: MyColors.themeColors[300],
+                  fontSize: 20.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          }
+          // Handle empty favorites
+          else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(
+              child: Text(
+                'No scores yet!',
+                style: TextStyle(
+                  color: MyColors.themeColors[300],
+                  fontSize: 20.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            padding: const EdgeInsets.all(10),
+            itemBuilder: (context, index) {
+              QuizScoreModel quizScore = snapshot.data![index];
+              return QuizScoreCard(quizScore: quizScore);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
